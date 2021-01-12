f109749ac138b2015ba6cbf21cd914e400c24b50
Sine <sinje@aarhus.dk>
2021-01-12 09:35:11 +0100

    PROJDB-7: small improvements

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,14 +6,21 @@ import Main from "../components/Main/Main";
 import { useTranslate } from "react-translate";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faGlobe,
-  faBoxes,
-  faBolt,
-  faParachuteBox,
+  faGlobe as box1Icon,
+  faBoxes as box2Icon,
+  faBolt as box3Icon,
+  faParachuteBox as box4Icon,
 } from "@fortawesome/free-solid-svg-icons";
 
 function Home({ location }) {
   const t = useTranslate("home");
+
+  const layout = [
+    { title: t("box1.title"), description: t("box1.description"), icon: box1Icon },
+    { title: t("box2.title"), description: t("box2.description"), icon: box2Icon },
+    { title: t("box3.title"), description: t("box3.description"), icon: box3Icon },
+    { title: t("box4.title"), description: t("box4.description"), icon: box4Icon }
+  ]
 
   return (
     <Layout location={location}>
@@ -39,72 +46,24 @@ function Home({ location }) {
 
             <div className="mt-10">
               <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-                <div className="flex">
-                  <div className="flex-shrink-0">
-                    <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                      <FontAwesomeIcon icon={faGlobe} className="text-white" />
+                {layout.map(box => (
+                  <div className="flex">
+                    <div className="flex-shrink-0">
+                      <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                        <FontAwesomeIcon icon={box.icon} className="text-white" />
+                      </div>
+                    </div>
+                    <div className="ml-4">
+                      <dt className="text-lg leading-6 font-medium text-gray-900">
+                        {box.title}
+                      </dt>
+                      <dd className="mt-2 text-base text-gray-500">
+                        {box.description}
+                      </dd>
                     </div>
                   </div>
-                  <div className="ml-4">
-                    <dt className="text-lg leading-6 font-medium text-gray-900">
-                      {t("box1.title")}
-                    </dt>
-                    <dd className="mt-2 text-base text-gray-500">
-                      {t("box1.description")}
-                    </dd>
-                  </div>
-                </div>
-
-                <div className="flex">
-                  <div className="flex-shrink-0">
-                    <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                      <FontAwesomeIcon icon={faBoxes} className="text-white" />
-                    </div>
-                  </div>
-                  <div className="ml-4">
-                    <dt className="text-lg leading-6 font-medium text-gray-900">
-                      {t("box2.title")}
-                    </dt>
-                    <dd className="mt-2 text-base text-gray-500">
-                      {t("box2.description")}
-                    </dd>
-                  </div>
-                </div>
-
-                <div className="flex">
-                  <div className="flex-shrink-0">
-                    <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                      <FontAwesomeIcon icon={faBolt} className="text-white" />
-                    </div>
-                  </div>
-                  <div className="ml-4">
-                    <dt className="text-lg leading-6 font-medium text-gray-900">
-                      {t("box3.title")}
-                    </dt>
-                    <dd className="mt-2 text-base text-gray-500">
-                      {t("box3.description")}
-                    </dd>
-                  </div>
-                </div>
-
-                <div className="flex">
-                  <div className="flex-shrink-0">
-                    <div className="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
-                      <FontAwesomeIcon
-                        icon={faParachuteBox}
-                        className="text-white"
-                      />
-                    </div>
-                  </div>
-                  <div className="ml-4">
-                    <dt className="text-lg leading-6 font-medium text-gray-900">
-                      {t("box4.title")}
-                    </dt>
-                    <dd className="mt-2 text-base text-gray-500">
-                      {t("box4.description")}
-                    </dd>
-                  </div>
-                </div>
+                  ))
+                }
               </dl>
             </div>
           </div>
