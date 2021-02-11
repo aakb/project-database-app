1255af077addaa7eae6379503e7257dc5dee5a62
Sine <sinje@aarhus.dk>
2021-02-11 09:27:48 +0100

    PROJDB-30: added container for ganttchart

---
+++ src/components/Chart/Gantt/GanttChartView.jsx
@@ -0,0 +1,36 @@
+import React, { useContext, useEffect, useState } from 'react'
+import AppStateContext from '../../../context/appStateContext'
+import Alert from '../../Alert/Alert'
+import GanttChart from './GanttChart'
+import './GanttChartView.css'
+import { useTranslate } from 'react-translate'
+import { mapGanttData, getEndDateForXAxis } from '../../utils/DataMapper'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSpinner } from '@fortawesome/free-solid-svg-icons'
+function GanttChartView () {
+  const t = useTranslate('chart')
+  const context = useContext(AppStateContext)
+  const [data, setData] = useState()
+  const endDateForXAxis = getEndDateForXAxis()
+  useEffect(() => {
+    setData(mapGanttData(context.data.get.data))
+  }, [])
+
+  return (
+    <>
+      <div className='chart'>
+        <div className='chart-container'>
+          {!context.isLoading.get && !context.hasError.get && data && (
+            <GanttChart data={data} endDateForXAxis={endDateForXAxis} />
+          )}
+          {context.isLoading.get && (
+            <FontAwesomeIcon icon={faSpinner} size='lg' spin />
+          )}
+          {context.hasError.get && <Alert variant='red'>{t('alert.error')}</Alert>}
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default GanttChartView
